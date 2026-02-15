e3a11f40d9261e4886569a97dbd64097639f478c
huriselengecagin <selengecagin@gmail.com>
2026-02-15 16:01:38 +0300

    Configure SQLAlchemy engine, ensure crypto_prices table schema, append DataFrame to PostgreSQL

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,10 @@
+from xml.dom.expatbuilder import TEXT_NODE
+
 import requests
 import pandas as pd
 from pathlib import Path
-import sqlalchemy
+from sqlalchemy import create_engine, text, VARCHAR
+from sqlalchemy.dialects.mysql import NUMERIC
 
 print(pd.Timestamp.now())
 # CoinGecko API endpoint for Bitcoin price
@@ -15,13 +18,52 @@ df = pd.DataFrame(data)
 
 df['timestamp'] = pd.Timestamp.now()
 
-# Look at it
+
 print(df.head())
 
-file_path = Path('crypto_prices.csv')
-if file_path.exists():
-    df.to_csv(file_path, header=False, mode='a',index=False)
-else:
-    df.to_csv(file_path, header=True, mode='w',index=False)
+engine = create_engine('postgresql+psycopg2://huriselengecagin:113308Monet@localhost/crypto_data_pipeline')
+
+# Create table if it doesn't exist
+with engine.connect() as conn:
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS crypto_prices (
+        id VARCHAR,
+        symbol VARCHAR,
+        name TEXT,
+        image TEXT,
+        current_price NUMERIC,
+        market_cap NUMERIC,
+        market_cap_rank NUMERIC,
+        fully_diluted_valuation NUMERIC,
+        total_volume NUMERIC,
+        high_24h NUMERIC,
+        low_24h NUMERIC,
+        price_change_24h NUMERIC,
+        price_change_percentage_24h NUMERIC,
+        market_cap_change_24h  NUMERIC,
+        market_cap_change_percentage_24h NUMERIC,
+        circulating_supply NUMERIC,
+        total_supply NUMERIC,
+        max_supply NUMERIC,
+        ath NUMERIC,
+        ath_change_percentage NUMERIC,
+        ath_date TIMESTAMP,
+        atl NUMERIC,
+        atl_change_percentage NUMERIC,
+        atl_date TIMESTAMP,
+        roi TEXT,
+        last_updated TIMESTAMP,
+        timestamp TIMESTAMP
+        )
+    """))
+df['roi'] = df['roi'].astype(str)
+
+df.to_sql('crypto_prices', if_exists='append',index=False,con=engine)
+
+# file_path = Path('crypto_prices.csv')
+# if file_path.exists():
+#     df.to_csv(file_path, header=False, mode='a',index=False)
+# else:
+#     df.to_csv(file_path, header=True, mode='w',index=False)
 
 
