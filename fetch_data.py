d6335cf974b16a3444b22f0f3d2ec9f510223f09
huriselengecagin <selengecagin@gmail.com>
2026-02-18 21:01:05 +0300

    Add error handling to database operations

--- fetch_data.py
+++ fetch_data.py
@@ -3,7 +3,10 @@ import requests
 import pandas as pd
 import logging
 from pathlib import Path
+
+import sqlalchemy
 from sqlalchemy import create_engine, text, VARCHAR
+from sqlalchemy.exc import DBAPIError
 
 logging.basicConfig(
     filename='pipeline.log',
@@ -30,54 +33,54 @@ except requests.exceptions.RequestException as errex:
     logging.error("Unexpected request exception occurred")
     sys.exit()
 
-
 # Convert to DataFrame
 df = pd.DataFrame(data)
 df['timestamp'] = pd.Timestamp.now()
+df['roi'] = df['roi'].astype(str)
 
 engine = create_engine('postgresql+psycopg2://huriselengecagin:113308Monet@localhost/crypto_data_pipeline')
 
-# Create table if it doesn't exist
-with engine.connect() as conn:
-    conn.execute(text("""
-        CREATE TABLE IF NOT EXISTS crypto_prices (
-        id VARCHAR,
-        symbol VARCHAR,
-        name TEXT,
-        image TEXT,
-        current_price NUMERIC,
-        market_cap NUMERIC,
-        market_cap_rank NUMERIC,
-        fully_diluted_valuation NUMERIC,
-        total_volume NUMERIC,
-        high_24h NUMERIC,
-        low_24h NUMERIC,
-        price_change_24h NUMERIC,
-        price_change_percentage_24h NUMERIC,
-        market_cap_change_24h  NUMERIC,
-        market_cap_change_percentage_24h NUMERIC,
-        circulating_supply NUMERIC,
-        total_supply NUMERIC,
-        max_supply NUMERIC,
-        ath NUMERIC,
-        ath_change_percentage NUMERIC,
-        ath_date TIMESTAMP,
-        atl NUMERIC,
-        atl_change_percentage NUMERIC,
-        atl_date TIMESTAMP,
-        roi TEXT,
-        last_updated TIMESTAMP,
-        timestamp TIMESTAMP
-        )
-    """))
-df['roi'] = df['roi'].astype(str)
+try:
+    with engine.connect() as conn:
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS crypto_prices (
+            id VARCHAR,
+            symbol VARCHAR,
+            name TEXT,
+            image TEXT,
+            current_price NUMERIC,
+            market_cap NUMERIC,
+            market_cap_rank NUMERIC,
+            fully_diluted_valuation NUMERIC,
+            total_volume NUMERIC,
+            high_24h NUMERIC,
+            low_24h NUMERIC,
+            price_change_24h NUMERIC,
+            price_change_percentage_24h NUMERIC,
+            market_cap_change_24h  NUMERIC,
+            market_cap_change_percentage_24h NUMERIC,
+            circulating_supply NUMERIC,
+            total_supply NUMERIC,
+            max_supply NUMERIC,
+            ath NUMERIC,
+            ath_change_percentage NUMERIC,
+            ath_date TIMESTAMP,
+            atl NUMERIC,
+            atl_change_percentage NUMERIC,
+            atl_date TIMESTAMP,
+            roi TEXT,
+            last_updated TIMESTAMP,
+            timestamp TIMESTAMP
+            )
+        """))
+except DBAPIError:
+    logging.critical('Failed to create table in database.')
+    sys.exit()
 
-df.to_sql('crypto_prices', if_exists='append',index=False, con=engine)
-
-# file_path = Path('crypto_prices.csv')
-# if file_path.exists():
-#     df.to_csv(file_path, header=False, mode='a',index=False)
-# else:
-#     df.to_csv(file_path, header=True, mode='w',index=False)
-
+try:
+    df.to_sql('crypto_prices', if_exists='append',index=False, con=engine)
+    logging.info('Successfully appended records to crypto_prices table')
+except DBAPIError:
+    logging.critical('Failed to append dataframe to crypto_prices')
+    sys.exit()
 
